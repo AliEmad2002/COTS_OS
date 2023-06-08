19afae7fcdf5d5042a0364e2993c0643885a272a
AliEmad2002 <ali.20352473@eng.aun.edu.eg>
2023-06-09 00:18:46 +0300

    crated HAL_OS

---
+++ Inc/HAL_OS.h
@@ -0,0 +1,15 @@
+/*
+ * HAL_OS.h
+ *
+ *  Created on: Jun 8, 2023
+ *      Author: Ali Emad
+ */
+
+#ifndef HAL_OS_INC_HAL_OS_H_
+#define HAL_OS_INC_HAL_OS_H_
+
+void vHOS_init(void);
+
+
+
+#endif /* HAL_OS_INC_HAL_OS_H_ */
---
+++ Port/Port_DIO.h
@@ -0,0 +1,104 @@
+/*
+ * Port_DIO.h
+ *
+ *  Created on: Jun 8, 2023
+ *      Author: Ali Emad
+ */
+
+#ifndef HAL_OS_PORT_PORT_DIO_H_
+#define HAL_OS_PORT_PORT_DIO_H_
+
+/*
+ * DIO driver of target MCU is included here, and its proper functions are called
+ * inside the following defined wrapper functions.
+ *
+ * If such a function is not provided by the available driver, user may write it
+ * inside the wrapper function (if too long to be inlined, remove the "inline"
+ * identifier).
+ */
+#include "stm32f103xb.h"
+#include "stm32f1xx_hal.h"
+
+/*
+ * Driver-long needed values.
+ *
+ * Notes:
+ * 		-	Values which are not used in the current target can be deleted.
+ * 		-	If additional values are needed, they can be added here.
+ */
+static GPIO_TypeDef* const portArr[] = {GPIOA, GPIOB, GPIOC};
+
+/*
+ * Initializes pin as digital input.
+ *
+ * Notes:
+ * 		-	"ucPull" has three states:
+ * 			0 ==> no pull
+ * 			1 ==> pull up
+ * 			2 ==> pull down
+ *
+ * 		-	DIO clock (if controlled) must be initially enabled.
+ */
+void vHOS_DIO_initPinInput(uint8_t ucPortNumber, uint8_t ucPinNumber, uint8_t ucPull)
+{
+	GPIO_InitTypeDef conf = {
+		.Pin = 1 << ucPinNumber,
+		.Mode = GPIO_MODE_INPUT,
+		.Pull = ucPull,
+		.Speed = GPIO_SPEED_FREQ_HIGH
+	};
+
+	HAL_GPIO_Init(portArr[ucPortNumber], &conf);
+}
+
+/*
+ * Initializes pin as digital output.
+ *
+ * Notes:
+ * 		-	DIO clock (if controlled) must be initially enabled.
+ */
+void vHOS_DIO_initPinOutput(uint8_t ucPortNumber, uint8_t ucPinNumber)
+{
+	GPIO_InitTypeDef conf = {
+		.Pin = 1 << ucPinNumber,
+		.Mode = GPIO_MODE_OUTPUT_PP,
+		.Pull = GPIO_NOPULL,
+		.Speed = GPIO_SPEED_FREQ_HIGH
+	};
+
+	HAL_GPIO_Init(portArr[ucPortNumber], &conf);
+}
+
+/*
+ * Reads pin input value
+ *
+ * Notes:
+ * 		-	Pin must be initially initialized as digital input.
+ *
+ *		-	Returns 0 if pin level is low, 1 if high.
+ *
+ * 		-	DIO clock (if controlled) must be initially enabled.
+ */
+inline __attribute__(( always_inline))
+uint8_t ucHOS_DIO_readPin(uint8_t ucPortNumber, uint8_t ucPinNumber)
+{
+	return HAL_GPIO_ReadPin(portArr[ucPortNumber], 1 << ucPinNumber);
+}
+
+/*
+ * Writes pin output value
+ *
+ * Notes:
+ * 		-	Pin must be initially initialized as digital output.
+ *
+ *		-	"state": 0 for low level, 1 for high level.
+ *
+ * 		-	DIO clock (if controlled) must be initially enabled.
+ */
+inline __attribute__(( always_inline))
+void vHOS_DIO_WritePin(uint8_t ucPortNumber, uint8_t ucPinNumber, uint8_t ucState)
+{
+	HAL_GPIO_WritePin(portArr[ucPortNumber], 1 << ucPinNumber, ucState);
+}
+
+#endif /* HAL_OS_PORT_PORT_DIO_H_ */

6a6678f84c3a1ac7f0ee679e05127121107e3c89
AliEmad2002 <ali.20352473@eng.aun.edu.eg>
2023-06-09 12:41:59 +0300

    Create Port_Breakpoint.h

---
+++ Port/Port_Breakpoint.h
@@ -0,0 +1,26 @@
+/*
+ * Port_Breakpoint.h
+ *
+ *  Created on: Jun 9, 2023
+ *      Author: Ali Emad
+ */
+
+#ifndef HAL_OS_PORT_PORT_BREAKPOINT_H_
+#define HAL_OS_PORT_PORT_BREAKPOINT_H_
+
+/*
+ * Breakpoint instruction (or its inline function/macro) is written inside the
+ * following wrapper.
+ *
+ * If the target MCU does not support breakpoints, the wrapper is left empty.
+ */
+
+
+#define vHOS_Breakpoint()		\
+{                               \
+	asm volatile("BKPT #0");    \
+}
+
+
+
+#endif /* HAL_OS_PORT_PORT_BREAKPOINT_H_ */

81a59a4a27be8f02d25e137f45909b0117784cdd
AliEmad2002 <ali.20352473@eng.aun.edu.eg>
2023-09-25 11:58:41 +0300

    Update Heap_Config.h

--- Inc/LIB/Heap/Heap_Config.h
+++ Inc/LIB/Heap/Heap_Config.h
@@ -12,11 +12,6 @@
  * This macro compares two elements of the type "xCONF_VECTOR_ELEM_TYPE".
  * It evaluates to 1 if "xElem1" considered of more priority to be closer to heap's
  * top than "xElem2". Otherwise, it evaluates to 0.
- *
- * As this driver is so-far used only in HWTime, it is much faster to do the comparison
- * in a macro than a callback function. Anyways, if this driver is to be used in other
- * code, it may be copied and changed per the new other code, or it may be modified
- * and added a comparison callback function.
  */
 #define ucCONF_HEAP_COMPARE(xElem1, xElem2)		\
 	(	(xElem1) <= (xElem2)	)

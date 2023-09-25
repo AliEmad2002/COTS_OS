812498d3d7b97dc1da0c3bedc61ba005f4b95423
AliEmad2002 <ali.20352473@eng.aun.edu.eg>
2023-09-25 03:42:34 +0300

    Deprecated the heap based delay
    * It took too much code size (Timer driver, not heap. Heap is actually very light).
    
    * Results were of ~ 150us minimum delay, which was not the goal of creating the driver.

--- Inc/HAL/HWTime/HWTime.h
+++ Inc/HAL/HWTime/HWTime.h
@@ -19,17 +19,6 @@
 
 #include "HAL/HWTime/HWTime_config.h"
 
-typedef struct{
-	/*		PRIVATE		*/
-
-	/*	Time at which the delay ends	*/
-	uint64_t ulReleaseTimestamp;
-
-	/*	Binary semaphore that gets locked at start of the delay, unlocked at the end of it	*/
-	SemaphoreHandle_t xSemaphore;
-	StaticSemaphore_t xSemaphoreStatic;
-}xHOS_HWTime_Delay_t;
-
 
 /*
  * Initializes driver.
@@ -76,26 +65,6 @@ uint64_t ulHOS_HWTime_getTimestampFromISR(void);
  */
 #define ulHOS_HWTime_TICKS_TO_US(ulTicks)	\
 	(((uint64_t)(ulTicks) * (uint64_t)1000000) / uiHOS_HWTIME_TIMER_FREQ_ACTUAL)
-
-/*
- * Initialize Delay handle.
- *
- * Notes:
- * 		-	This must be globally defined (In order for the HWTime code to be able
- * 			to deal with it when the task using it is not running).
- */
-void vHOS_HWTime_initDelay(xHOS_HWTime_Delay_t* pxHandle);
-
-/*
- * Configures delay handle.
- *
- * Notes:
- * 		-	The passed handle must not be currently in use (waiting for a previous
- * 			delay to end).
- */
-void vHOS_HWTime_delay(xHOS_HWTime_Delay_t* pxHandle, uint32_t uiTicks);
-
-
 
 
 
--- Src/HAL/HWTime.c
+++ Src/HAL/HWTime.c
@@ -22,7 +22,6 @@
 /*	SELF	*/
 #include "HAL/HWTime/HWTime.h"
 
-#define uiCONF_HWTIME_MAX_NUMBER_OF_DELAYS		50
 
 /*******************************************************************************
  * Static / Global variables:
@@ -31,20 +30,6 @@ static uint32_t uiOvfCount = 0;
 
 static uint32_t uiMaxCounterVal;
 
-static xLIB_Heap_t xHeap;
-
-static StaticTask_t xTaskStatic;
-static TaskHandle_t xTask;
-static StackType_t xTaskStack[configMINIMAL_STACK_SIZE];
-
-static SemaphoreHandle_t xCaptureCompareSemaphore;
-static StaticSemaphore_t xCaptureCompareSemaphoreStatic;
-
-static SemaphoreHandle_t xConfigCountingSemaphore;
-static StaticSemaphore_t xConfigCountingSemaphoreStatic;
-
-static SemaphoreHandle_t xHeapSizeSemaphore;
-static StaticSemaphore_t xHeapSizeSemaphoreStatic;
 
 /*******************************************************************************
  * Callbacks:
@@ -55,108 +40,6 @@ static void vOvfCallback(void* pvParams)
 	uiOvfCount++;
 }
 
-static void vCcCallback(void* pvParams)
-{
-	vPort_TIM_disableCcInterrupt(ucHOS_HWTIME_TIMER_UNIT_NUMBER);
-
-	BaseType_t xHighPriorityTaskWoken = pdFALSE;
-	xSemaphoreGiveFromISR(	xCaptureCompareSemaphore,
-							&xHighPriorityTaskWoken	);
-
-	portYIELD_FROM_ISR(xHighPriorityTaskWoken);
-}
-
-/*******************************************************************************
- * RTOS task function:
- ******************************************************************************/
-static void vTask(void* pvParams)
-{
-	SemaphoreHandle_t* pxCurrentRunningDelaySemaphore = NULL;
-
-	xHOS_HWTime_Delay_t* pxHeapTop;
-
-	while(1)
-	{
-		/*	If compare unit is currently configured for a delay	*/
-		if (xSemaphoreTake(xConfigCountingSemaphore, 0))
-		{
-			/*	Block until capture/compare interrupt occurs	*/
-			xSemaphoreTake(xCaptureCompareSemaphore, portMAX_DELAY);
-
-			/*	Give semaphore of the just ended delay	*/
-			xSemaphoreGive(*pxCurrentRunningDelaySemaphore);
-			//taskYIELD();
-		}
-
-		/*
-		 * Block until heap has elements in it.
-		 */
-		xSemaphoreTake(xHeapSizeSemaphore, portMAX_DELAY);
-		xSemaphoreGive(xHeapSizeSemaphore);
-
-		/*	Lock the heap	*/
-		ucLIB_HEAP_LOCK(&xHeap, portMAX_DELAY);
-
-		/*	Get pointer to heap's top delay handle	*/
-		pxHeapTop = (xHOS_HWTime_Delay_t*)pxLIB_HEAP_GET_TOP_VAL(&xHeap);
-
-		/*
-		 * If heap's top time has already passed, release its semaphore and remove
-		 * it from the heap.
-		 */
-		if (pxHeapTop->ulReleaseTimestamp <= ulHOS_HWTime_getTimestamp())
-		{
-			xSemaphoreGive(pxHeapTop->xSemaphore);
-			//taskYIELD();
-
-			vLIB_Heap_removeTop(&xHeap);
-			xSemaphoreTake(xHeapSizeSemaphore, 0);
-		}
-
-		/*
-		 * Otherwise, if heap's top time has not yet passed and is in the current
-		 * OVF iteration.
-		 */
-		else if (pxHeapTop->ulReleaseTimestamp / uiMaxCounterVal == uiOvfCount)
-		{
-			/*	Configure HW timer compare unit with it	*/
-			vPort_TIM_writeOcRegister(
-				ucHOS_HWTIME_TIMER_UNIT_NUMBER,
-				pxHeapTop->ulReleaseTimestamp % uiMaxCounterVal	);
-
-			xSemaphoreGive(xConfigCountingSemaphore);
-			vPORT_TIM_CLEAR_CC_FLAG(ucHOS_HWTIME_TIMER_UNIT_NUMBER);
-			vPort_TIM_enableCcInterrupt(ucHOS_HWTIME_TIMER_UNIT_NUMBER);
-
-			/*	If heap's top time has passed while being configured	*/
-			if (pxHeapTop->ulReleaseTimestamp <= ulHOS_HWTime_getTimestamp())
-			{
-				/*	Give its semaphore immediately	*/
-				xSemaphoreGive(pxHeapTop->xSemaphore);
-				//taskYIELD();
-
-				/*	De-configure HW timer compare unit.	*/
-				vPort_TIM_disableCcInterrupt(ucHOS_HWTIME_TIMER_UNIT_NUMBER);
-				vPORT_TIM_CLEAR_CC_FLAG(ucHOS_HWTIME_TIMER_UNIT_NUMBER);
-				xSemaphoreTake(xCaptureCompareSemaphore, 0);
-				xSemaphoreTake(xConfigCountingSemaphore, 0);
-			}
-
-			/*
-			 * Otherwise, store pointer to its semaphore to be released when delay
-			 * ends.
-			 */
-			pxCurrentRunningDelaySemaphore = &pxHeapTop->xSemaphore;
-
-			/*	Remove heap's top	*/
-			vLIB_Heap_removeTop(&xHeap);
-			xSemaphoreTake(xHeapSizeSemaphore, portMAX_DELAY);
-		}
-
-		/*	Release the heap	*/
-		vLIB_HEAP_RELEASE(&xHeap);
-	}
-}
 
 /*******************************************************************************
  * Helping functions / macros:
@@ -180,8 +63,6 @@ static inline void vInitHWTimer(void)
 
 	vPort_TIM_setOvfCallback(ucTimerUnitNumber, vOvfCallback, NULL);
 
-	vPort_TIM_setCcCallback(ucTimerUnitNumber, vCcCallback, NULL);
-
 	vPort_TIM_enableOverflowInterrupt(ucTimerUnitNumber);
 
 	vPort_TIM_setCountingDirUp(ucTimerUnitNumber);
@@ -200,53 +81,8 @@ static inline void vInitInterruptController()
 		configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY);
 
 	vPort_Interrupt_enableIRQ(pxPortInterruptTimerOvfIrqNumberArr[ucTimerUnitNumber]);
-
-	vPort_Interrupt_setPriority(
-		pxPortInterruptTimerCcIrqNumberArr[ucTimerUnitNumber],
-		configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY);
-
-	vPort_Interrupt_enableIRQ(pxPortInterruptTimerCcIrqNumberArr[ucTimerUnitNumber]);
 }
 
-static inline void vInitDelayController()
-{
-	/*	Initialize the heap	*/
-	vLIB_Heap_init(&xHeap);
-
-	/*	Initialize capture/compare semaphore	*/
-	xCaptureCompareSemaphore = xSemaphoreCreateBinaryStatic(&xCaptureCompareSemaphoreStatic);
-	xSemaphoreTake(xCaptureCompareSemaphore, 0);
-
-	/*	Initialize configuration counting semaphore	*/
-	xConfigCountingSemaphore = xSemaphoreCreateCountingStatic(
-		uiCONF_HWTIME_MAX_NUMBER_OF_DELAYS,
-		0,
-		&xConfigCountingSemaphoreStatic);
-
-	/*	Initialize heap size counting semaphore	*/
-	xHeapSizeSemaphore = xSemaphoreCreateCountingStatic(
-		uiCONF_HWTIME_MAX_NUMBER_OF_DELAYS,
-		0,
-		&xHeapSizeSemaphoreStatic);
-
-	/*	Initialize task	*/
-	xTask = xTaskCreateStatic(	vTask,
-								"HWTime",
-								configMINIMAL_STACK_SIZE,
-								NULL,
-								configHOS_HWTIME_TASK_PRI,
-								xTaskStack,
-								&xTaskStatic	);
-}
-
-static inline ucIsThereActiveDelay(void)
-{
-	uint8_t ucIsThereActiveDelay = xSemaphoreTake(xConfigCountingSemaphore, 0);
-	if (ucIsThereActiveDelay)
-		xSemaphoreGive(xConfigCountingSemaphore);
-
-	return ucIsThereActiveDelay;
-}
 
 /*******************************************************************************
  * API functions:
@@ -261,9 +97,6 @@ void vHOS_HWTime_init(void)
 
 	/*	Initialize interrupt	*/
 	vInitInterruptController();
-
-	/*	Initialize delay controller SW	*/
-	vInitDelayController();
 }
 
 /*
@@ -356,55 +189,6 @@ uint64_t ulHOS_HWTime_getTimestampFromISR(void)
 	return ulTimestamp;
 
 }
-
-/*
- * See header for info.
- */
-void vHOS_HWTime_initDelay(xHOS_HWTime_Delay_t* pxHandle)
-{
-	/*	Create semaphore	*/
-	pxHandle->xSemaphore = xSemaphoreCreateBinaryStatic(&pxHandle->xSemaphoreStatic);
-
-	/*	Initially available	*/
-	xSemaphoreGive(pxHandle->xSemaphore);
-}
-
-/*
- * See header for info.
- */
-void vHOS_HWTime_delay(xHOS_HWTime_Delay_t* pxHandle, uint32_t uiTicks)
-{
-	/*	Calculate time at which delay ends	*/
-	pxHandle->ulReleaseTimestamp = ulHOS_HWTime_getTimestamp() + uiTicks;
-
-	/*	If there's a currently active configured delay	*/
-	if (ucIsThereActiveDelay())
-	{
-		/*	If new delay ends before the currently configured delay	*/
-		//TODO
-	}
-
-
-	/*	Assure delay's semaphore is not available	*/
-	(void)xSemaphoreTake(pxHandle->xSemaphore, 0);
-
-	/*	Add to the heap	*/
-	ucLIB_HEAP_LOCK(&xHeap, portMAX_DELAY);
-
-	int32_t iHandleAddress = (int32_t)pxHandle;
-	vLIB_Heap_addElem(&xHeap, &iHandleAddress);
-
-	xSemaphoreGive(xHeapSizeSemaphore);
-
-	vLIB_HEAP_RELEASE(&xHeap);
-
-	/*	Lock until semaphore is released	*/
-	xSemaphoreTake(pxHandle->xSemaphore, portMAX_DELAY);
-
-	/*	Give it back (To notify other functions that delay has ended)	*/
-	xSemaphoreGive(pxHandle->xSemaphore);
-}
-
 
 
 
